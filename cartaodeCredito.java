1ad7692460db0befd5c092f7539379f13713dbd4
Guilherme-Vieira-Souza <guilhermesilvasouza8@gmail.com>
2024-10-11 11:34:06 -0300

    Add files via upload

---
+++ cartaodeCredito.java
@@ -0,0 +1,32 @@
+package CARTAO;
+public class cartaodeCredito {
+        
+    int numero;
+    String numeroDoTitular;
+    String cpf;
+    double limite;
+    double fatura;
+    
+    public double consultarLimite(){
+        return limite;
+    }
+    public double consultarSaldo(){
+        return fatura;
+    }
+
+    public void realizarCompra(double valor){
+    if(valor <= this.limite){
+        this.limite -= valor;
+        this.fatura += valor;
+        System.out.println("Parabéns pela sua compra!!! Vc não é PROBRE!!!");
+    }else{
+        System.out.println("Voce não tem limite para concluir sua compra, SEU LAZARENTO PROBRE!!!!");
+        System.out.println("Seu limite é de : "+ this.limite);
+    }
+    }
+    
+}
+    
+
+    
+
